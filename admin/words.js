b35304a5661802c20ffb3f98e519ff70268e8ddf
Sebastian Schwill <privat@konzeptplus.net>
2021-02-13 18:02:19 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"ble-position adapter settings": {
+		"en": "Adapter settings for ble-position",
+		"de": "Adaptereinstellungen für ble-position",
+		"ru": "Настройки адаптера для ble-position",
+		"pt": "Configurações do adaptador para ble-position",
+		"nl": "Adapterinstellingen voor ble-position",
+		"fr": "Paramètres d'adaptateur pour ble-position",
+		"it": "Impostazioni dell'adattatore per ble-position",
+		"es": "Ajustes del adaptador para ble-position",
+		"pl": "Ustawienia adaptera dla ble-position",
+		"zh-cn": "ble-position的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
